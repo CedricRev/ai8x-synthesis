aa83d3df4e3ef1e1d35dc37362712b959c8c1682
CedricRev <53487258+CedricRev@users.noreply.github.com>
2024-11-15 17:21:04 +0800

    Personal Scripts

--- scripts/gen_catsdogs_max78000.sh
+++ scripts/gen_catsdogs_max78000.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 DEVICE="MAX78000"
-TARGET="sdk/Examples/$DEVICE/CNN"
-COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
+TARGET="D:/MaximSDK/Examples/$DEVICE/CNN"
+COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose" 
+QUANTIZED_MODEL = "../ai8x-training/logs/2024.11.15-151510/best_quantized.pth.tar"
+YAML = "networks/cats-dogs-hwc.yaml"
 
-python ai8xize.py --test-dir $TARGET --prefix cats-dogs --checkpoint-file trained/ai85-catsdogs-qat8-q.pth.tar --config-file networks/cats-dogs-hwc.yaml --fifo --softmax $COMMON_ARGS "$@"
+python ai8xize.py --test-dir $TARGET --prefix cats-dogs_gen --overwrite --checkpoint-file $QUANTIZED_MODEL --config-file $YAML --fifo --softmax $COMMON_ARGS "$@"
--- scripts/quantize_catsdogs.sh
+++ scripts/quantize_catsdogs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python quantize.py trained/ai85-catsdogs-qat8.pth.tar trained/ai85-catsdogs-qat8-q.pth.tar --device MAX78000 -v "$@"
+python quantize.py "../ai8x-training/logs/2024.11.15-151510/best.pth.tar" "../ai8x-training/logs/2024.11.15-151510/best_quantized.pth.tar" --device MAX78000 -v "$@"
